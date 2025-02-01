d34acd72d60d3c5ec8f55049f5154e65b52eb247
DevanshBajpai09 <devanshbajpai09@gmail.com>
2025-02-01 23:44:38 +0530

    'commit'

--- faq_project/settings.py
+++ faq_project/settings.py
@@ -18,8 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Activate Django-Heroku
-django_heroku.settings(locals())
+
 
 # Static files settings
 STATIC_URL = '/static/'
